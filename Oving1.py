ce1917368f9a5bda23d01868bb8f552d9017d974
Tri Tac Le <tritac.le@gmail.com>
2025-08-22 10:12:55 +0200

    Added: plotting and cleaned up terminaloutput

--- Oving1.py
+++ Oving1.py
@@ -1,6 +1,6 @@
 import random
 import timeit
-
+import matplotlib.pyplot as plt 
 """
 Helpers
 """
@@ -53,13 +53,14 @@ def get_highest_return(data: list) -> int | None:
     if current_return > highest_return: 
       highest_return = current_return 
       entry_day = current_idx
-      sell_day = i + 1
+      sell_day = i+1
   ## Return operasjon
   return highest_return, entry_day, sell_day
 
 def oppgave1():
   # kursforandringer oppgitt i oppgaven
   data = [-1, 3, -9, 2, 2, -1, 2, -1, -5]
+  print(f"Aksjeendringer: {data}")
   h, b, s = get_highest_return(data)
   print(f"Max return: {h}, entry on day: {b}, and sell on day: {s}")
 
@@ -91,11 +92,35 @@ def timer_tracking(lst: list, rounds: int) -> float:
   return (t2-t1)/rounds
 
 def oppgave3():
-  n_values = [1000, 10000, 100000, 1000000, 10000000]
+  n_values = [10**e for e in range(3,8)]
+  print(f"tallrekkeverdier: {n_values}")
+  time_values = []
   for n in n_values:
     lst = get_random_int_lst(n)
-    time = timer_tracking(lst, 5)
-    print(f"n_value: {n}, time: {time}")
+    time = timer_tracking(lst, len(n_values))
+    time_values.append(time)
+    print(f"n_value: {n}, time: {time} seconds")
+  
+  # Plotting
+  #plt.plot(time_values, n_values)
+  plt.scatter(time_values, n_values)
+  plt.xlabel("time")
+  plt.ylabel("n_values")
+  plt.show()
+
+"""
+Målingen viser en lineær forhold mellom tid og størrelsen på n. Algoritmen er som nevnt ovenfor er algortmen Θ(n) og dermed lineær. 
+n-verdiene valgt er en tidobling av n > 1000. I terminaloutput og plottingen ser vi at en tidobling av n, gir omtrent tidobling av kjøretid. 
+Dette bekrefter min analyse i oppgave 1-2 at algoritmen er Θ(n)
+
+Terminaloutput:
+tallrekkeverdier: [1000, 10000, 100000, 1000000, 10000000]
+n_value: 1000, time: 9.637200273573398e-05 seconds
+n_value: 10000, time: 0.0009391811909154058 seconds
+n_value: 100000, time: 0.00985900501254946 seconds
+n_value: 1000000, time: 0.09542412641458213 seconds
+n_value: 10000000, time: 1.0165447901934386 seconds
+"""
 
 def oppgave_chooser(number: int) -> None:
   match number:
@@ -107,13 +132,12 @@ def oppgave_chooser(number: int) -> None:
       oppgave3()
     case _:
       print("choose a number between 1 and 3")
-      
+
 if __name__ == "__main__":
-  user_input = input("Choose oppgave\n").strip()
+  user_input = input("Choose oppgave (number between 1 and 3)\n").strip()
   try:
     choice = int(user_input)
   except ValueError:
     print("choose a number between 1 and 3\n")
   else:
     oppgave_chooser(choice)
-  
