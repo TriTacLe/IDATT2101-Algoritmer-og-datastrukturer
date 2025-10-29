41f5f0fda32a6cbaa8f7b2ee214e51a90ad9936c
Tri Tac Le <tritac.le@gmail.com>
2025-10-29 14:27:38 +0100

    feat: found file size

--- Oving6/komprimering.c
+++ Oving6/komprimering.c
@@ -1,16 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main()
+long int findSize(char fileName[])
 {
   FILE *filePointer;
 
-  // oblig file
-  filePointer = fopen("diverse.lyx", "w");
-  // filePointer = fopen("Twenty_thousand_leagues_under_the_sea.txt", "w");
-  if (filePointer == NULL)
-    exit(0);
+  filePointer = fopen(fileName, "rb");
 
+  if (filePointer == NULL)
+  {
+    printf("File does not exist");
+    return -1;
+  }
+
+  fseek(filePointer, 0, SEEK_END);
+
+  long int fileSize = ftell(filePointer);
   fclose(filePointer);
+  return fileSize;
+}
+
+int main()
+{
+  // oblig file
+  char fileName[] = {"diverse.lyx"};
+  long int res = findSize(fileName);
+  (res != -1) ? printf("File size: %ld bytes\n", res) : printf("File is emtpy\n");
   return 0;
 }
