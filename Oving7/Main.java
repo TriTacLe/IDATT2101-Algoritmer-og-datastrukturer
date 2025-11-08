2aa4112d42dcb9a476062b6fd1aea83774d6645d
Tri Tac Le <tritac.le@gmail.com>
2025-11-08 13:39:14 +0100

    Nav file added

--- Oving7/Main.java
+++ Oving7/Main.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,16 +18,57 @@ enum InteressePunkter{
 }
 
 public class Main{
+  public static final class Node{
+    public final int nodenr;
+    public final double latitude;
+    public final double longitude;
+    public Node(int nodenr, double latitute, double longitude){
+      this.nodenr = nodenr;
+      this.latitude = latitute;
+      this.longitude=longitude;
+    }
+  }
+
+  public static final class Edge{
+    public final int toNodeNr;
+    public final int fromNodeNr;
+    public final int travelTime; // Kjøretid
+    // public final int length;
+    // public final int speedLimit;
+    public Edge(int toNodeNr, int fromNodeNr, int travelTime){
+      this.toNodeNr = toNodeNr;
+      this.fromNodeNr = fromNodeNr;
+      this.travelTime = travelTime;
+    }
+  }
+
+  public static final class Graph{
+    
+  }
+
+  public static final class InteressePunkt{
+    public final int nodenr;
+    public final int kode;
+    public final String stedsnavn;
+    
+    public InteressePunkt(int nodenr, int kode, String stedsnavn){
+      this.nodenr = nodenr;
+      this.kode = kode;
+      this.stedsnavn = stedsnavn;
+    }
+  }
+  
   private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
   static void readFile(String filePathString) throws IOException {
     Path filePath = Paths.get(filePathString);
     try (BufferedReader bufferedReader =
               Files.newBufferedReader(filePath, StandardCharsets.UTF_8)) {
-        String lineText;
-        while ((lineText = bufferedReader.readLine()) != null) {
-            System.out.println(lineText);
-        }
+        
     }
+  }
+
+  static int[] dijktra(int totalNodes, List<Edge> edges, Node srcNode){
+    return 
   }
 
   private static InteressePunkter getInteressepunkter(int code){
