06dcbec40ff79a80036f7ceb3425b2dbf74f4893
Shakti Om Sharma <shaktiforgithub@gmail.com>
2025-11-08 13:48:55 +0100

    made a class for the nodes

---
+++ Oving7/Node.java
@@ -0,0 +1,12 @@
+public final class Node {
+
+    public final int id;
+    public final float lat;  // float holder i massevis her
+    public final float lon;
+
+    public Node(int id, float lat, float lon) {
+        this.id = id;
+        this.lat = lat;
+        this.lon = lon;
+    }
+}
